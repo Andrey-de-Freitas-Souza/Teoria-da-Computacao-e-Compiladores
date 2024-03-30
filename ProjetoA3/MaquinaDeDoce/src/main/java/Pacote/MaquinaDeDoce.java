a62d926f6586b9d3f230d3a46feb5857fc6eacc2
AndreyFSouza <andreyfsouzaa@gmail.com>
2024-03-30 18:54:14 -0300

    update

---
+++ ProjetoA3/MaquinaDeDoce/src/main/java/Pacote/MaquinaDeDoce.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Pacote;
+
+/**
+ *
+ * @author andre
+ */
+public class MaquinaDeDoce {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
